32f62b4db2d5a93621f4a4c8ecb79b6692bd8b1f
giancarlo22 <giancarlo.zevallos.lecca@gmail.com>
2017-03-10 10:20:35 -0500

    Add files via upload

---
+++ Deve-lovers/View/v_inicio.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Developer UTP</title>
+        <link href="http://localhost:8080/Deve-lovers/library/css/bootstrap.min.css" rel="stylesheet">
+        <link href="http://localhost:8080/Deve-lovers/library/css/estilos.css" rel="stylesheet">
+        <link href="../library/css/style.css" rel="stylesheet" type="text/css"/>
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <header class="col-xs-12">
+                <div class="col-xs-4 text-center" id="logo">
+                    <a href="#" class=""><img src="http://localhost:8080/Deve-lovers/images/logo-dc.png"></a>
+                </div>
+                <div class="col-xs-8 text-center medio" id="nom_pag"><br><br>DEVELOPERS COMMUNITY UTP</div>
+                <div class="col-xs-12" id="menu">
+                    <div class="content">
+                        <h1 class="title">Slide Down Box Menu with jQuery and CSS3</h1>
+                        <ul id="sdt_menu" class="sdt_menu">
+                            <li>
+                                <a href="#">
+                                    <img src="../images/2.jpg" alt=""/>
+                                    <span class="sdt_active"></span>
+                                    <span class="sdt_wrap">
+                                        <span class="sdt_link">About me</span>
+                                        <span class="sdt_descr">Get to know me</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../images/1.jpg" alt=""/>
+                                    <span class="sdt_active"></span>
+                                    <span class="sdt_wrap">
+                                        <span class="sdt_link">Portfolio</span>
+                                        <span class="sdt_descr">My work</span>
+                                    </span>
+                                </a>
+                                <div class="sdt_box">
+                                    <a href="#">Websites</a>
+                                    <a href="#">Illustrations</a>
+                                    <a href="#">Photography</a>
+                                </div>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../images/3.jpg" alt=""/>
+                                    <span class="sdt_active"></span>
+                                    <span class="sdt_wrap">
+                                        <span class="sdt_link">Inspiration</span>
+                                        <span class="sdt_descr">Where ideas get born</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../images/4.jpg" alt=""/>
+                                    <span class="sdt_active"></span>
+                                    <span class="sdt_wrap">
+                                        <span class="sdt_link">Photos</span>
+                                        <span class="sdt_descr">I like to photograph</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../images/5.jpg" alt=""/>
+                                    <span class="sdt_active"></span>
+                                    <span class="sdt_wrap">
+                                        <span class="sdt_link">Blog</span>
+                                        <span class="sdt_descr">I write about design</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../images/6.jpg" alt=""/>
+                                    <span class="sdt_active"></span>
+                                    <span class="sdt_wrap">
+                                        <span class="sdt_link">Projects</span>
+                                        <span class="sdt_descr">I like to code</span>
+                                    </span>
+                                </a>
+                                <div class="sdt_box">
+                                    <a href="#">Job Board Website</a>
+                                    <a href="#">Shopping Cart</a>
+                                    <a href="#">Interactive Maps</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </header>
+            <section>
+                <aside class="col-xs-3">
+
+                </aside>
+                <div class="col-xs-6">
+
+                </div>
+                <article class="col-xs-3">
+
+                </article>
+            </section>
+            <footer></footer>
+        </div>
+
+        <script src="../library/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="../library/js/bootstrap.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script src="../library/js/jquery.easing.1.3.js" type="text/javascript"></script>
+        <script src="../library/js/menu.js" type="text/javascript"></script>
+       
+    </body>
+</html>
